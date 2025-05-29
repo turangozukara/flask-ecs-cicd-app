4a0b02e21dd0003371061179b05f3b0a8e92fbe4
Turan Gozukara <turangozukara@outlook.com>
2025-05-29 16:13:13 +0300

    updated

--- app/app.py
+++ app/app.py
@@ -2,6 +2,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route('/')
 def hello():
-   return "Hello from Flask on ECS v1"
+   return "Hello from Flask on ECS v2"
 if __name__ == '__main__':
    app.run(host='0.0.0.0', port=5000)
